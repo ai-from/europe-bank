fea6f369d2eb977c345f6307ee80ceff49c02dc1
Aifrom <codemusic2020@gmail.com>
2020-07-21 23:18:44 +0300

    order card form - base behavior

--- app/blocks/orderCardForm/order-card-form.js
+++ app/blocks/orderCardForm/order-card-form.js
@@ -1,7 +1,16 @@
 var orderCard = new Vue({
   el: '#order-card-form',
   data: {
-
+    fio: '',
+    email: '',
+    phone: '',
+    citizenship: '',
+    fioStatus: false,
+    emailStatus: false,
+    phoneStatus: false,
+    citizenshipStatus: false,
+    checkbox: false,
+    checkForm: false
   },
   methods: {
     closeForm(){
@@ -10,6 +19,17 @@ var orderCard = new Vue({
     },
     labelClick(e){
       e.target.previousElementSibling.focus()
+    },
+    checkboxClick(){
+      this.checkbox = !this.checkbox
+    },
+    focusInput(){
+      document.querySelector('.dropdown').classList.add('show')
+      document.querySelector('.dropdown-icon').classList.add('rotate')
+    },
+    blurInput(){
+      document.querySelector('.dropdown').classList.remove('show')
+      document.querySelector('.dropdown-icon').classList.remove('rotate')
     }
   }
 })
