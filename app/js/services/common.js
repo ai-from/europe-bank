8dd1e4f700b0f1572166fe55da50daef08adec13
Aifrom <codemusic2020@gmail.com>
2020-07-21 13:48:47 +0300

    add scrolling to

--- app/js/services/common.js
+++ app/js/services/common.js
@@ -1,3 +1,11 @@
+// burger click
 $('.burger').on('click', function(){
     $('.all').hasClass('menu') ? $('.all').removeClass('menu') : $('.all').addClass('menu')
 })
+
+// scrolling to
+$('a[href^="#"]').on('click', function(){
+    var el = $(this).attr('href');
+    $('html').animate({scrollTop: $(el).offset().top}, 2000);
+    return false;
+});
