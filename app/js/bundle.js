e525a9c5709361104ee886207b690eaa2606d69f
Aifrom <codemusic2020@gmail.com>
2020-07-23 03:59:24 +0300

    github fix

---
+++ app/js/bundle.js
@@ -0,0 +1,10 @@
+// script_libs
+//= script_libs.js
+
+$(document).ready(function(){
+
+	//= services/common.js
+  //= ../blocks/orderCard/order-card.js
+  //= ../blocks/orderCardForm/order-card-form.js
+
+});
---
+++ app/js/script_libs.js
@@ -0,0 +1,5 @@
+/*! html5shiv.js */
+//= libs/html5shiv.js
+
+/*! jquery.min.js */
+//= libs/jquery.min.js
